cc5e5d6d30c000619caaaac06bd400c92ce453e4
Mandalorian7773 <adityajagrani8@gmail.com>
2024-12-08 23:27:53 +0530

    defined element

---
+++ script.js
@@ -0,0 +1,5 @@
+const toDoForm = document.getElementById('container1');
+const toDoInput = document.getElementById('todo-Input');
+const timeInput = document.getElementById('time');
+const ListContainer = document.querySelector('max-container');
+

51e18301471103d6670e8538b6c2c6ef0667cea4
Mandalorian7773 <adityajagrani8@gmail.com>
2024-12-08 23:38:38 +0530

    added addTask Function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ toDoForm.addEventListener('submit', (event) => {
 
 })
 
+
+function addTask (task,time){
+    const taskBox = document.createElement('div');
+    taskBox.classList.add('container2');
+
+    const taskTitle = document.createElement('h3');
+
+    taskTitle.textContent = task;
+
+    const taskTime = document.createElement('p');
+    taskTime.textContent =`Time: ${time};` 
+
+
+    taskBox.appendChild(taskTitle);
+    taskBox.appendChild(taskTime);
+
+
+}
+

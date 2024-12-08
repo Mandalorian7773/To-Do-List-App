e476d1433eb29080cce1f0e6638577216c81042a
Mandalorian7773 <adityajagrani8@gmail.com>
2024-12-08 23:31:52 +0530

    added eventListner to form

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const toDoInput = document.getElementById('todo-Input');
 const timeInput = document.getElementById('time');
 const ListContainer = document.querySelector('max-container');
 
+
+toDoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input= toDoInput.Value.trim();
+    const time = timeInput.Value.trim();
+    if( input == '' ||  time == ''){
+        alert('Please fill in all fields!');
+        return;
+    }
+
+})
+

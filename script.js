f5bdf907b4a35a5e9f8183a8e22a7c42b46f48c0
Mandalorian7773 <adityajagrani8@gmail.com>
2024-12-08 23:52:37 +0530

    completed the js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const toDoForm = document.getElementById('container1');
 const toDoInput = document.getElementById('todo-Input');
 const timeInput = document.getElementById('time');
-const ListContainer = document.querySelector('max-container');
+const ListContainer = document.querySelector('.max-container');
 
 
 toDoForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const input= toDoInput.Value.trim();
-    const time = timeInput.Value.trim();
+    const input= toDoInput.value.trim();
+    const time = timeInput.value.trim();
     if( input == '' ||  time == ''){
         alert('Please fill in all fields!');
         return;
     }
+
+
+    addTask(input,time);
+    toDoInput.value = '';
+    timeInput.value = '';
 
 })
 
@@ -25,12 +30,14 @@ function addTask (task,time){
     taskTitle.textContent = task;
 
     const taskTime = document.createElement('p');
-    taskTime.textContent =`Time: ${time};` 
+    taskTime.textContent =`Time: ${time}` ;
 
 
     taskBox.appendChild(taskTitle);
     taskBox.appendChild(taskTime);
 
+    ListContainer.style.display='flex'
+    ListContainer.appendChild(taskBox);
 
 }
 
